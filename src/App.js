07aa2c3dfa73ad12f3ff0b8817e24a4de866cc63
Sunny <shubelislam.diu@gmail.com>
2021-09-19 19:40:23 +0600

    react core concepts details

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
+// First Example
+// function App() {
+//   return (
+//     <div>
+//       <Person name= 'Shubel' job= 'Programmer'></Person>
+//       <Person name= 'Islam' job= 'Developer'></Person>
+//       <Person name= 'Sunny' job ='Engineer'></Person>
+//     </div>
+//   )
+// }
+
+// function Person(props) {
+//   const styleColor = {
+//     backgroundColor: 'hotpink',
+//     color: 'white',
+//     margin: '10px',
+//     padding: '20px',
+//     borderRadius: '20px'
+//   }
+//   return (
+//     <div style= {styleColor}>
+//       <h1>Name: {props.name}</h1>
+//       <h3>Job: {props.job}</h3>
+//     </div>
+//   )
+// }
+
+// Second Example
 function App() {
+  const styleColor = {
+    backgroundColor: 'lightseagreen',
+    margin: '10px',
+    padding: '20px',
+    border: '5px solid yellow',
+    borderRadius: '20px'
+  }
+  const cinemas = [
+    { nayok: 'Koober', nayika: 'kopila' },
+    { nayok: 'Rubel', nayika: 'Moushumi' },
+    { nayok: 'Razzak', nayika: 'Shabana' },
+    { nayok: 'Jashim', nayika: 'Suchorita' }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styleColor}>
+      {
+        cinemas.map(cinema => <Cinema nayok={cinema.nayok} nayika={cinema.nayika}></Cinema>)
+      }
     </div>
-  );
+  )
+}
+
+function Cinema(props) {
+  return (
+    <div>
+      <h2>Nayok: {props.nayok}</h2>
+      <h4>Nayika: {props.nayika}</h4>
+    </div>
+  )
 }
 
 export default App;
